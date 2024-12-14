02a2fa329212813608eb952ce247ae6d9b4b86f0
chui <tschui7745@gmail.com>
2024-12-14 21:34:48 +0800

    update

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ data "aws_availability_zones" "available" {
 module "vpc" {
  source  = "terraform-aws-modules/vpc/aws"
 
- name = "tschui-example-vpc"
+ name = "tschui-vpc"
  cidr = "10.0.0.0/16"
 
  azs             = data.aws_availability_zones.available.names
